3fbdecc59f592752802204753cecabf686d6e5a1
mullerow <maximilian.kohler@posteo.de>
2024-07-07 16:21:40 +0200

    mgradient and the y-shift of the tiles are now dynamicly generated

--- src/components/resourcesScreen.vue
+++ src/components/resourcesScreen.vue
@@ -2,8 +2,14 @@
   <div>
     <div class="display-ressource-container">
       <h3>Ressourcen</h3>
-      Kohlenhydrate: 50 g || Lipide: 30 g || Proteine: 10 g || sec. Stoffe: 0 g <br />
-      Wasser 40 g || Phosphor: 0.2 g || Stickstoff: 4 g
+      Kohlenhydrate: <span class="amount-resource"> 50 g </span> || Lipide:
+      <span class="amount-resource">30 g </span> || Proteine:
+      <span class="amount-resource">10 g </span> || sec. Stoffe:
+      <span class="amount-resource">0 g</span> <br />
+      Wasser <span class="amount-resource">40 g</span> || Phosphor:
+      <span class="amount-resource">0.2 g </span> || Stickstoff:
+      <span class="amount-resource">4 g </span>|| Energie:
+      <span class="amount-resource">81 KJ</span>
     </div>
   </div>
 </template>
@@ -15,5 +21,8 @@
   background-color: silver;
   z-index: 3;
   height: 70px;
+}
+.amount-resource {
+  background-color: aliceblue;
 }
 </style>
--- src/stores/store.js
+++ src/stores/store.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import soilGroundImage from '@/assets/images/ground-soil.png'
 import skyImage from '@/assets/images/blue-sky.png'
 import seedling50 from '@/assets/images/seedling50x50.png'
-import stemLvl1 from '@/assets/images/stem-lvl1-2.png'
+import stemLvl1 from '@/assets/images/stems/stem-lvl1-2.png'
 import leafLvl1Left from '@/assets/images/leafs/leaf-lvl1-left-1.png'
 import leafLvl1right from '@/assets/images/leafs/leaf-lvl1-right-1.png'
 import leafLvl1straight from '@/assets/images/leafs/leaf-lvl1-straight1.png'
@@ -18,54 +18,27 @@ export const storeData = defineStore('store', {
   }),
   actions: {
     createHexagons() {
+      let additionalIncreaseOfYShift = 0
+      let brightness = 1.7
       for (let y = 1; y <= this.playgroundData.amountRows; y++) {
         let xShiftOfHexagon = 13
+        additionalIncreaseOfYShift -= 10
+        if (y <= 9) {
+          brightness -= 0.1
+        } else {
+          brightness = 1
+        }
         for (let x = 1; x <= this.playgroundData.amountColumns; x++) {
           // verschieben der hexagone, sodass diese eine lückenlose Fläch ergeben
           let yShiftOfHexagon = 0
           let backgroundImageHexagon = ''
           let hexagonType = ''
-          let brightness = 1
+
           xShiftOfHexagon -= 13
           if (x % 2 === 0) {
             yShiftOfHexagon += 22
           }
-          if (y === 1) {
-            yShiftOfHexagon -= 10
-            brightness = 1.7
-          } else if (y === 2) {
-            yShiftOfHexagon -= 20
-            brightness = 1.6
-          } else if (y === 3) {
-            yShiftOfHexagon -= 30
-            brightness = 1.5
-          } else if (y === 4) {
-            yShiftOfHexagon -= 40
-            brightness = 1.4
-          } else if (y === 5) {
-            yShiftOfHexagon -= 50
-            brightness = 1.3
-          } else if (y === 6) {
-            yShiftOfHexagon -= 60
-            brightness = 1.2
-          } else if (y === 7) {
-            yShiftOfHexagon -= 70
-            brightness = 1.1
-          } else if (y === 8) {
-            yShiftOfHexagon -= 80
-            brightness = 1.0
-          } else if (y === 9) {
-            yShiftOfHexagon -= 90
-            brightness = 0.9
-          } else if (y === 10) {
-            yShiftOfHexagon -= 100
-          } else if (y === 11) {
-            yShiftOfHexagon -= 110
-          } else if (y === 12) {
-            yShiftOfHexagon -= 120
-          } else if (y === 13) {
-            yShiftOfHexagon -= 130
-          }
+          yShiftOfHexagon = yShiftOfHexagon + additionalIncreaseOfYShift
           // hinzufügen der hintergrundbilder
           if (y < 10) {
             backgroundImageHexagon = skyImage
